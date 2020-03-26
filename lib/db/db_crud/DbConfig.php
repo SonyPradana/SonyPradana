7b7e1d5feb74ad94902c88aef9e9ef946a824f02
Angger Pradana <sonypradana@gmail.com>
2020-03-26 19:37:37 +0700

    penambahan .gitignore list

--- lib/db/db_crud/DbConfig.php
+++ lib/db/db_crud/DbConfig.php
@@ -8,8 +8,9 @@ class DbConfig{
     private $_conn;
     // 45.118.132.253
     // 203.114.74.128
-    public function __construct($database_name = "simpusle_simpus_lerep"){      
-        $conn = mysqli_connect("45.118.132.253", "simpusle_admin", "ulfamylove", $database_name);
+    public function __construct($database_name = "simpusle_simpus_lerep"){              
+        $conn = mysqli_connect("localhost", "root", "", $database_name);
+        // = mysqli_connect("45.118.132.253", "simpusle_admin", "ulfamylove", $database_name);
         $this->_conn = $conn;        
     }
     public function StartConnection(){
