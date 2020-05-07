da91ea30313e9f98191037e581aa0ca1782e51a2
Angger Pradana <sonypradana@gmail.com>
2020-05-07 22:02:14 +0700

    penambahan page about dan our team [feat][fix]
    -penambahan halaman about dan halaman our-team
    -memperbaiki bug box-account yg  menamapilkan modal walau tdk login

---
+++ p/about/index.php
@@ -0,0 +1,157 @@
+<?php
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/apps/init.php';
+?>
+<?php 
+    #Aunt cek
+    session_start();
+    $token = (isset($_SESSION['token']) ) ? $_SESSION['token'] : '';
+    $auth = new Auth($token, 2);
+    $user = new User($auth->getUserName());
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta content="id" name="language">
+    <meta content="id" name="geo.country">
+    <meta http-equiv="content-language" content="In-Id">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tentang Kami</title>
+    <meta name="description" content="Tentang kami">
+    <meta name="keywords" content="simpus lerep, puskesmas lerep, puskesmas, aboutus, about">
+    <meta name="author" content="amp">
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/include/html/metatag.html') ?>
+
+    <link rel="stylesheet" href="/lib/css/main.css">
+    <link rel="stylesheet" href="/lib/css/ui/v1/control.css">    
+    <link rel="stylesheet" href="/lib/css/ui/v1/timeline.css">
+    <link rel="stylesheet" href="/lib/css/ui/v1/alert.css">
+    <script src="/lib/js/index.js"></script>
+    <script src="/lib/js/bundles/keepalive.js"></script>
+    <style>
+        .container{
+            display: grid;
+            grid-template-columns: 1fr minmax(250px, 280px);
+            grid-column-gap: 24px; grid-row-gap: 24px;
+        }
+        main{ margin: 24px}
+        article .about p{
+            line-height: 1.5;
+            font-size: 1.3rem;
+            margin: 4px 0;
+        }
+        @media screen and (max-width: 600px) {
+            .container{grid-template-columns: 1fr}
+        }
+    </style>
+</head>
+<body>    
+    <header>
+        <?php $active_menu = 'home' ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/lib/components/header/header.html') ?>
+    </header>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/lib/components/control/modal.html') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/lib/components/control/alert.html') ?>
+    
+    <div class="container">
+        <main>
+            <div class="coit breadcrumb">
+                <ul class="crumb">
+                    <li><a href="/">Home</a></li>
+                    <li>Tentang Kami</li>
+                </ul>
+            </div>
+            <div class="about-boxs">
+                <div class="about-image">
+                    <img src="/data/img/logo/logo-puskesmas.png" alt="logo simpus lerep" width="200">
+                </div>
+                <article class="about-contain">
+                    <div class="about">
+                        <h2>Simpus Lerep</h2>
+                        <p>SIMPUS, merupakan program aplikasi yang memberikan informasi baik untuk administrasi dan pengelolaan sebuah puskesmas demi meningkatkan kinerja dan menangani keseluruhan proses manajemen di Puskesmas</p>
+                    
+                        <h2>Hubungi Kami</h2>
+                        <p>Kami sangat gembira menerima chat Anda. Sangat bagus apablia ingin memberikan masukan dengan email: <a href="mailto:support@simpuslerep.com">support@simpuslerep.com</a></p>
+                        <h2>Kontribusi Bersama</h2>
+                        <p>Untuk semua Pegawai puskesmas Lerep dapat ikut perkontribusi langsung melalui Registrasi langsung di <a href="/p/auth/register/">Register User Baru</a></p>
+                    </div>
+                    <div class="sparator blue">
+                        <div class="sparator-title"><h3>Lini Masa</h3></div>
+                    </div>
+                    <div class="boxs-timeline">
+                        <ul class="time-line">
+                            <li class="story">
+                                <div class="time">21 Maret 2020</div>
+                                <div class="message">
+                                    <p>I was born, Website pertama kali di publish</p>
+                                </div>
+                            </li>
+                            <li class="story">
+                                <div class="time">28 Maret 2020</div>
+                                <div class="message">
+                                    <p>Data Rekam Medis Pertama dibuat</p>
+                                </div>
+                            </li>
+                            <li class="story">
+                                <div class="time">30 Maret 2020</div>
+                                <div class="message">
+                                    <p>Logo dan Merek dibuat</p>
+                                </div>
+                            </li>
+                            <li class="story">
+                                <div class="time">1 April 2020</div>
+                                <div class="message">
+                                    <p>Penambahan fitur filter di Lihat Data Rekam Medis</p>
+                                </div>
+                            </li>
+                            <li class="story">
+                                <div class="time">8 April 2020</div>
+                                <div class="message">
+                                    <p>Perbaikan Search Result (Hasil Pencaria) Data Rekam Medis lebih tepat / relevant</p>
+                                </div>
+                            </li>
+                            <li class="story">
+                                <div class="time">22 April 2020</div>
+                                <div class="message">
+                                    <p>Pemberithuan(Alert) bila seeson logintelah berahir</p>
+                                </div>
+                            </li>
+                            <li class="story">
+                                <div class="time">1 May 2020</div>
+                                <div class="message">
+                                    <p>Penambahan fitur Ulasan atau Review untuk pelayanan Kami, berseta laporan kritik dan saran.</p>
+                                </div>
+                            </li>
+                            <li class="story">
+                                <div class="time">7 May 2020</div>
+                                <div class="message">
+                                    <p>Peningkatan Keamanan authorization dan authentication dari injection.</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </atricle>
+            </div>
+        </main>
+        <aside class="right-side">
+
+        </aside>
+    </div>
+
+    <div class="gotop" onclick="gTop()"></div>
+    <?php if( isset( $msg ) ) :?>
+        <div class="snackbar">
+            <?= $msg ?>
+        </div>
+    <?php endif; ?>
+    <footer>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/lib/components/footer/footer.html') ?>
+    </footer>
+</body>
+<script>    
+    // sticky header
+    window.onscroll = function(){stickyHeader('82px', '32px')};
+    var mycontent = document.querySelector('.container');
+</script>
+<script src="/lib/js/index.end.js"></script>
+</html>
---
+++ p/contact/ourteam/index.php
@@ -0,0 +1,127 @@
+<?php
+    require_once $_SERVER['DOCUMENT_ROOT'] . '/lib/apps/init.php';
+?>
+<?php 
+    #Aunt cek
+    session_start();
+    $token = (isset($_SESSION['token']) ) ? $_SESSION['token'] : '';
+    $auth = new Auth($token, 2);
+    $user = new User($auth->getUserName());
+?>
+<?php
+    $db = new MyPDO();
+    $db->query('SELECT `display_name`, `section` FROM `profiles`');
+    $result = $db->resultset();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta content="id" name="language">
+    <meta content="id" name="geo.country">
+    <meta http-equiv="content-language" content="In-Id">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tim Kita</title>
+    <meta name="description" content="TTeam Kita">
+    <meta name="keywords" content="simpus lerep, puskesmas lerep, puskesmas, aboutus, about">
+    <meta name="author" content="amp">
+<?php include($_SERVER['DOCUMENT_ROOT'] . '/include/html/metatag.html') ?>
+
+    <link rel="stylesheet" href="/lib/css/main.css">
+    <link rel="stylesheet" href="/lib/css/ui/v1/control.css">    
+    <link rel="stylesheet" href="/lib/css/ui/v1/card.css">
+    <link rel="stylesheet" href="/lib/css/ui/v1/alert.css">
+    <script src="/lib/js/index.js"></script>
+    <script src="/lib/js/bundles/keepalive.js"></script>
+    <style>
+        .container{
+            display: grid;
+            grid-template-columns: 1fr minmax(250px, 280px);
+            grid-column-gap: 24px; grid-row-gap: 24px;
+        }
+        main{ margin: 24px}
+        article .about p{
+            line-height: 1.5;
+            font-size: 1.3rem;
+            margin: 4px 0;
+        }
+
+        .team-boxs .box.header{
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            width: 100%
+        }
+        .team-boxs .box.cards{
+            width: auto; height: auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .team-card{margin: 0 16px}
+
+        @media screen and (max-width: 600px) {
+            .container{grid-template-columns: 1fr}
+        }
+        @media screen and (max-width: 347px) {
+            .team-boxs .box.cards{flex-direction: column}
+            .team-card{margin: 16px 0}
+        }
+    </style>
+</head>
+<body>    
+    <header>
+        <?php $active_menu = 'home' ?>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/lib/components/header/header.html') ?>
+    </header>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/lib/components/control/modal.html') ?>
+    <?php include($_SERVER['DOCUMENT_ROOT'] . '/lib/components/control/alert.html') ?>
+    
+    <div class="container">
+        <main>
+            <div class="coit breadcrumb">
+                <ul class="crumb">
+                    <li><a href="/">Home</a></li>
+                    <li>Contact</li>
+                    <li>Team Kita</li>
+                </ul>
+            </div>
+            <div class="team-boxs">
+                <div class="box header"><h1>Our Team</h1></div>
+                <div class="box cards">
+                    <?php foreach ($result as $value) :?>
+                    <div class="team-card">
+                        <div class="card image">
+                            <img src="/data/img/display-picture/no-image.png" alt="profile ">
+                        </div>
+                        <div class="card content">
+                            <div class="title"><?= $value['display_name'] ?></div>
+                            <div class="subtitle"><?= $value['section'] ?></div>
+                        </div>
+                    </div>
+                    <?php endforeach ;?>
+                </div>
+            </div>
+        </main>
+        <aside class="right-side">
+
+        </aside>
+    </div>
+
+    <div class="gotop" onclick="gTop()"></div>
+    <?php if( isset( $msg ) ) :?>
+        <div class="snackbar">
+            <?= $msg ?>
+        </div>
+    <?php endif; ?>
+    <footer>
+        <?php include($_SERVER['DOCUMENT_ROOT'] . '/lib/components/footer/footer.html') ?>
+    </footer>
+</body>
+<script>    
+    // sticky header
+    window.onscroll = function(){stickyHeader('82px', '32px')};
+    var mycontent = document.querySelector('.container');
+</script>
+<script src="/lib/js/index.end.js"></script>
+</html>
