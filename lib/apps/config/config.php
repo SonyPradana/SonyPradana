14835d6159d92680f979fa8ca74604d292a6bdef
Angger Pradana <sonypradana@gmail.com>
2020-08-19 22:27:48 +0700

    dukunan kembali file config.php [refactor]
    - mencegah error ketika konfigurasi baru tidak ter-update ke web hosting
    - sebagai base load konfigurasi aplikasi

---
+++ lib/apps/config/config.php
@@ -0,0 +1,13 @@
+<?php 
+
+define('BASEURL', $_SERVER['DOCUMENT_ROOT']);
+date_default_timezone_set('Asia/Jakarta');
+
+// DB local
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+// DB remote
+// define('DB_HOST', '45.118.132.253');
+// define('DB_USER', 'simpusle_admin');
+// define('DB_PASS', 'ulfamylove');
