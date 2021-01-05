86c8d62a999b9bf0c2703314260aef20835a2b0a
Angger Pradana <sonypradana@gmail.com>
2021-01-05 18:04:19 +0700

    fix: issue API when upload data using POST method

--- public/index.php
+++ public/index.php
@@ -115,7 +115,7 @@ $app->match(['get', 'post'], '/kia-anak/(:text)/(:text)', function(string $actio
 });
 
 // API
-$app->match(['get', 'put'], '/API/([0-9a-zA-Z.]*)/(:any)/(:any).json', function($version, $unit, $action) {
+$app->match(['get', 'put', 'post'], '/API/([0-9a-zA-Z.]*)/(:any)/(:any).json', function($version, $unit, $action) {
   (new ApiController())->index($unit, $action, $version);
 });
 // API - Mix
