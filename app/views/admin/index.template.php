43b2cee12b2ace02dbbf73138df7550a5903f34b
Angger Pradana <sonypradana@gmail.com>
2021-02-19 22:45:43 +0700

    fix: admin page style not reload
    - fix admin css link not start from base url

--- app/views/admin/index.template.php
+++ app/views/admin/index.template.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <?php include(APP_FULLPATH['component'] . 'meta/metatag.php') ?>
-  <link rel="stylesheet" href="vue/css/app.css">
+  <link rel="stylesheet" href="/vue/css/app.css">
 </head>
 <body class="antilaliased font-sans bg-gray-50 dark:bg-gray-800">
 
