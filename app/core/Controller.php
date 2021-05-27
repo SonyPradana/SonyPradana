941c70ae19919a3c3b246ce011080b77746e277b
Angger Pradana <sonypradana@gmail.com>
2021-05-27 18:49:54 +0700

    feat: controller support static call

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -3,10 +3,11 @@
 namespace Simpus\Apps;
 use Simpus\Apps\Middleware;
 
-class Controller extends Middleware
+abstract class Controller extends Middleware
 {
 
-  public function view($view, $portal = []){
+  public function view($view, $portal = [])
+  {
     // short hand to access content
     if (isset($portal['contents'])) {
       $content = (object) $portal['contents'];
@@ -22,20 +23,41 @@ class Controller extends Middleware
     return $this;
   }
 
-  public static function view_exists($view) :bool{
-      return file_exists( APP_FULLPATH['view'] . $view . '.template.php');
+  public static function view_exists($view) :bool
+  {
+    return file_exists( APP_FULLPATH['view'] . $view . '.template.php');
   }
 
-  public static function getController($contoller, $method, $args = []){
-      $contoller          = ucfirst($contoller) . 'Controller';
-      $contoller_location = APP_FULLPATH['controllers'] . $contoller . '.php';
-      if( file_exists($contoller_location) ){
-          require_once $contoller_location;
-          $controller_name = new $contoller;
-          if( method_exists($controller_name, $method) ){
-              call_user_func_array([$controller_name, $method], $args);
-              return;
-          }
+  /**
+   * Shorthand to create new controller
+   * @param string $controller Name of contorller
+   * @param array $args Paramter to pass controller contractor
+   * @return static Controller
+   */
+  public static function getController($contoller, $method, $args = [])
+  {
+    $contoller_location = APP_FULLPATH['controllers'] . $contoller . '.php';
+    if (file_exists($contoller_location)) {
+      require_once $contoller_location;
+      $controller_name = new $contoller;
+      if (method_exists($controller_name, $method)) {
+        call_user_func_array([$controller_name, $method], $args);
+        return;
       }
+    }
+  }
+
+  /**
+   * @var static This classs
+   */
+  private self $_static;
+
+  /**
+   * Instance of controller.
+   * Shorthadn to crete new class
+   */
+  public static function static()
+  {
+    return self::$_static ?? new static;
   }
 }
